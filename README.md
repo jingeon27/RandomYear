36a1637bc0218f1dc64ac221efe2bce4303c070b
jingeon27 <jingeon27@gmail.com>
2022-12-12 23:04:23 +0900

    ADD :: createRandomYear

---
+++ README.md
@@ -0,0 +1,7 @@
+# random날짜 생성
+
+```js
+const { YearRandom } = require("randdaymake");
+const ASDF = YearRandom(2022);
+// or const ASDF = YearRandom(2000,2022);
+```
---
+++ bin/index.js
@@ -0,0 +1,19 @@
+const YearRandom = (startDate, lastDate = startDate) => {
+  const count = lastDate - Math.floor(Math.random() * (lastDate - startDate));
+  const Month = Math.floor(Math.random() * 12) + 1;
+  const arr = [1, 3, 5, 7, 8, 10, 12];
+  let DayProps = arr.includes(Month)
+    ? 31
+    : Month !== 2
+    ? 30
+    : count % 4 === 0
+    ? count % 100 === 0
+      ? count % 400 === 0
+        ? 29
+        : 28
+      : 29
+    : 29;
+  const Date = Math.floor(Math.random() * DayProps) + 1;
+  return `${count}-${Month}-${Date}`;
+};
+exports.YearRandom = YearRandom;
